dc1ea183f22dad308b80fef04c045d97019a7adc
cholisky <cholisky@gmail.com>
2020-08-06 13:33:09 -0700

    a but of styling, wired up the stubs for the authProvider context/HOC

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,17 +1,52 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
-import { Text, Input, Button } from 'react-native-elements';
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Button, Input, Text } from 'react-native-elements';
+import Spacer from '../components/Spacer';
 
 const SignupScreen = ({ navigation }) => {
-  return <>
-      <Text h3>Sign Up for Tracker</Text>
-    <Input label="Email" />
-    <Input label="Password" />
-    <Button title="Sign Up" onPress={() => navigation.navigate('Signin')} />
-    <Button title="Go to Main Flow" onPress={() => navigation.navigate('mainFlow')} />
-    </>
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <View style={styles.container}>
+      <Spacer>
+        <Text h3>Sign Up for Tracker</Text>
+      </Spacer>
+      <Input
+        label="Email"
+        value={email}
+        onChangeText={setEmail}
+      autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <Spacer />
+      <Input
+        label="Password"
+        value={password}
+        onChangeText={setPassword}
+        autoCapitalize="none"
+        autoCorrect={false}
+        secureTextEntry
+      />
+      <Spacer>
+        <Button title="Sign Up" onPress={() => navigation.navigate('Signin')} />
+      </Spacer>
+    </View>
+  );
 };
 
-const styles = StyleSheet.create({});
+SignupScreen.navigationOptions = () => {
+  return {
+    header: () => false,
+  };
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom: 150,
+  }
+});
 
 export default SignupScreen;
